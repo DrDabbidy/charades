497dcf50c7916ebc6e6197e8f20d5914bcf581f4
David De Martin <david.demartin123@gmail.com>
2022-09-06 15:22:43 -0400

    Change timer to orange when active

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ let teamTwoTimeComponent = document.querySelector("#time-2")
 let team = true;
 
 // logic
+const changeTeam = () => {
+    if (team) {
+        teamOneTimeComponent.style.color = "#FF7F51";
+        teamTwoTimeComponent.style.color = "white";
+    } else {
+        teamOneTimeComponent.style.color = "white";
+        teamTwoTimeComponent.style.color = "#FF7F51";
+    }
+};
 const incrementTimer = () => {
     if (team) {
         teamOneTime += 1;
@@ -20,12 +29,14 @@ const incrementTimer = () => {
         teamTwoTime += 1;
         teamTwoTimeComponent.textContent = strftime(teamTwoTime);
     }
-}
+};
 let t = setInterval(incrementTimer, 1000);
+changeTeam();
 
 // add event listeners
 document.querySelector("#button").addEventListener("click", () => {
     team = !team;
+    changeTeam();
     clearInterval(t);
     t = setInterval(incrementTimer, 1000);
 });
